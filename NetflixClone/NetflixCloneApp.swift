5ecbeb41df01e9802414010cf973205149748273
Chavin2543 <rungallday@hotmail.com>
2022-02-10 03:27:06 +0700

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by chavin Panicharoen on 10/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
